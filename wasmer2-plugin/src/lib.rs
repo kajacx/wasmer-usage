927dc9d1cf2a8f50f4e4246d62a1377b48a072da
kajacx <kajacx@gmail.com>
2023-03-11 20:48:09 +0100

    Change lib type

--- wasmer2-plugin/src/lib.rs
+++ wasmer2-plugin/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
+pub fn add(left: u32, right: u32) -> u32 {
     left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
