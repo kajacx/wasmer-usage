37911b01f8fe88ff488e6891fbe701acb4e7bc1f
kajacx <kajacx@gmail.com>
2023-03-25 18:04:53 +0100

    Try plugin

--- wasmer-plugin/src/lib.rs
+++ wasmer-plugin/src/lib.rs
@@ -1,16 +1,69 @@
+use std::{alloc::*, sync::Mutex};
+
+static TEXT: Mutex<String> = Mutex::new(String::new());
+
 #[no_mangle]
-pub fn give_string(number: u32) -> u64 {
-    let text = format!(
-        "Hello, I have {} {}",
-        number,
-        if number == 1 { "apple" } else { "apples" }
-    );
+pub fn push_string(text: u64) -> u64 {
+    let bytes = import_from_host(text);
+    let text = String::from_utf8(bytes).unwrap();
 
-    let leaked = Box::leak(text.into_boxed_str());
+    TEXT.lock().unwrap().push_str(&text);
 
-    let len = leaked.len() as u64;
-    let address = leaked as *const str as *const () as usize as u64;
+    // FIXME: there is a bug. `export_to_host` takes vec, but that vec might have bigger capacity than size
+    // Let's see if the test uncovers it
+    let cloned = TEXT.lock().unwrap().as_bytes().to_owned();
+    export_to_host(cloned)
+}
 
-    // Address is in lower bytes, len in higher ones
-    (len << 32) | address
+#[no_mangle]
+pub fn remove_chars(num: u32) {
+    let len = TEXT.lock().unwrap().len();
+    TEXT.lock()
+        .unwrap()
+        .replace_range((len - num as usize)..len, "");
+}
+
+fn export_to_host(data: Vec<u8>) -> u64 {
+    let reference = data.leak();
+    let addr = reference as *const [u8] as *const u8 as usize;
+    to_fatptr(addr, reference.len())
+}
+
+fn import_from_host(fatptr: u64) -> Vec<u8> {
+    let (addr, len) = from_fatptr(fatptr);
+    // SAFETY: Host is giving us full ownership of these bytes
+    unsafe { Vec::from_raw_parts(addr as *mut u8, len, len) }
+}
+
+#[no_mangle]
+pub fn allocate_for_host(size: usize) -> u64 {
+    let addr = if size == 0 {
+        0
+    } else {
+        let layout = Layout::from_size_align(size, 1).unwrap();
+        // SAFETY: size is not zero
+        let bytes = unsafe { alloc(layout) };
+        bytes as *mut u8 as usize
+    };
+    to_fatptr(addr, size)
+}
+
+#[no_mangle]
+pub fn free_from_host(fatptr: u64) {
+    let (addr, len) = from_fatptr(fatptr);
+    if len != 0 {
+        let layout = Layout::from_size_align(len, 1).unwrap();
+        // SAFETY: size is not zero, and host guarantees to not use these bytes anymore
+        unsafe { dealloc(addr as *mut u8, layout) };
+    };
+}
+
+fn from_fatptr(fatptr: u64) -> (usize, usize) {
+    let addr = fatptr as u32 as usize;
+    let len = (fatptr << 32) as usize;
+    (addr, len)
+}
+
+fn to_fatptr(addr: usize, len: usize) -> u64 {
+    (addr as u64) << 32 | len as u64
 }
