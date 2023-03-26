f6e854a5e5b106981ceed008017a99b94f654606
kajacx <kajacx@gmail.com>
2023-03-26 11:19:10 +0200

    OH FUCK

--- wasmer3-runtime/src/main.rs
+++ wasmer3-runtime/src/main.rs
@@ -1,5 +1,3 @@
-use std::borrow::Borrow;
-
 use wasmer::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,10 +5,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "../../wasmer-plugin/target/wasm32-unknown-unknown/debug/wasmer_plugin.wasm"
     )
     .as_ref();
-
-    let fp = to_fatptr(420, 20);
-    let (addr, len) = from_fatptr(fp);
-    println!("ADDR: {addr}, LEN: {len}");
 
     // Create the store
     let store = Store::new(Cranelift::default());
@@ -24,7 +18,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut store = Store::new(Cranelift::default());
 
     // Create an empty import object.
-    let import_object = imports! {};
+    let import_object = imports! {
+        "my_imports" => {
+            "transform_string" => Function::new_typed(&mut store, |input: u64| {
+                let text = String::from_utf8(import_from_plugin(input));
+            })
+        }
+    };
 
     println!("Instantiating module...");
     // Let's instantiate the Wasm module.
@@ -34,9 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut compare_string = String::new();
 
-    // let mut shring_strings = |n: u32| {};
-
-    for i in 0..10_000_000u32 {
+    for i in 0..1_000_000u32 {
         if i % 1000 == 0 {
             println!("{}: {}", i, compare_string.len());
         }
@@ -46,13 +44,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             continue;
         }
         if compare_string.len() > 1000000 {
-            shrink_strings(memory, &mut store, &instance, &mut compare_string, i % 100);
+            shrink_strings(&mut store, &instance, &mut compare_string, i % 100);
             continue;
         }
         if i % 13 < 5 {
             grow_strings(memory, &mut store, &instance, &mut compare_string, i);
         } else {
-            shrink_strings(memory, &mut store, &instance, &mut compare_string, i % 100);
+            shrink_strings(&mut store, &instance, &mut compare_string, i % 100);
         }
     }
 
@@ -70,15 +68,11 @@ fn grow_strings(
     compare_string.push_str(&appendings);
     let exported = export_to_plugin(memory, store, instance, appendings.as_bytes());
 
-    // println!("Passing into plugin: {:?}", from_fatptr(exported));
-
     let push_str = instance
         .exports
         .get_typed_function::<u64, u64>(&store, "push_string")
         .unwrap();
     let ret = push_str.call(store, exported).unwrap();
-
-    // println!("Getting from  plugin: {:?}", from_fatptr(ret));
 
     let imported = import_from_plugin(instance, memory, store, ret);
     let check = String::from_utf8(imported).unwrap();
@@ -88,28 +82,13 @@ fn grow_strings(
     assert_eq!(a, b);
 }
 
-fn shrink_strings(
-    memory: &Memory,
-    store: &mut Store,
-    instance: &Instance,
-    compare_string: &mut String,
-    n: u32,
-) {
-    remove_chars(compare_string, n);
+fn shrink_strings(store: &mut Store, instance: &Instance, compare_string: &mut String, n: u32) {
+    shrink_string(compare_string, n);
     let rm_chars = instance
         .exports
         .get_typed_function::<u32, ()>(&store, "remove_chars")
         .unwrap();
     rm_chars.call(store, n).unwrap();
-}
-
-fn push_string(string: &mut String, text: &str) {
-    string.push_str(text);
-}
-
-fn remove_chars(string: &mut String, num: u32) {
-    let len = string.len();
-    string.replace_range((len - num as usize)..len, "");
 }
 
 fn import_from_plugin(
@@ -119,7 +98,6 @@ fn import_from_plugin(
     fatptr: u64,
 ) -> Vec<u8> {
     let (addr, len) = from_fatptr(fatptr);
-    // println!("addr: {addr}, len: {len}");
     let mut bytes = vec![0; len];
     let view = memory.view(store);
     view.read(addr as u64, &mut bytes[0..len]).unwrap();
@@ -141,7 +119,6 @@ fn export_to_plugin(memory: &Memory, store: &mut Store, instance: &Instance, dat
     let mut allocate = |size: u32| allocate.call(store, size).unwrap();
 
     let fatptr = allocate(data.len() as u32);
-    // println!("Allocated in host: {:?}", from_fatptr(fatptr));
     let (addr, _) = from_fatptr(fatptr);
     let view = memory.view(store);
     view.write(addr as u64, data).unwrap();
